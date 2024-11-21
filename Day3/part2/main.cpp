9e1c5e1f3352cb91e2073c3ee6c3b9f4caea5886
Grant Starkman <gstarkman@nex.io>
2024-11-21 16:00:28 -0700

    day3 done

--- Day3/part2/main.cpp
+++ Day3/part2/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 bool neighborContainsSymbol(const vector<vector<char>>&, int, int);
 bool isSafeToCheckNeighbor(int, int, const vector<vector<char>>&);
 bool isSymbol(char);
+string findSymbol(const vector<vector<char>>&, int, int);
 
 int main() {
   vector<vector<char>> matrix;
   string line;
   string possiblePartNumber;
+  unordered_map<string, vector<string>> map;
 
   while (getline(cin, line)) {
     matrix.push_back(vector<char>(line.begin(), line.end()));
@@ -19,30 +22,66 @@ int main() {
 
   int sum = 0;
   bool symbolFound = false;
+  string symbolCat = "";
 
   for (int i = 0; i < matrix.size(); i++) {
     for (int j = 0; j < matrix[i].size(); j++) {
       if (isdigit(matrix[i][j])) {
         possiblePartNumber += matrix[i][j];
         if (neighborContainsSymbol(matrix, i, j)) {
+          symbolCat = findSymbol(matrix, i, j);
           symbolFound = true;
-        } 
+        } else {
+          // cout << "Neighbor does not contain symbol " << matrix[i][j] << endl; 
+        }
       } else {
-        // matrix[i][j]  is not a digit, must be symbol or .
         if(matrix[i][j] == '.' || isSymbol(matrix[i][j])) {
-          if (possiblePartNumber != "" && symbolFound) {
-            cout << "Adding " << possiblePartNumber << " to sum" << endl;
-            sum += stoi(possiblePartNumber);
+          if (possiblePartNumber != "" && symbolFound && symbolCat != "") {
+            map[symbolCat].push_back(possiblePartNumber);
           }
-          // matrix[i][j] is a .
           symbolFound = false;
           possiblePartNumber = "";
+          symbolCat = "";
         }
+        symbolCat = "";
       }
     }
   }
+
+  for (auto it = map.begin(); it != map.end(); it++) {
+    int valueCount = 0;
+    int potentialMultiplier = 1;
+    cout << "Key: " << it->first << endl;
+    for (auto it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+      valueCount++;
+      potentialMultiplier *= stoi(*it2);
+      cout << "Value: " << *it2 << endl;
+    }
+    cout << endl;
+    if(valueCount == 2) {
+      cout << "Potential Multiplier: " << potentialMultiplier << endl;
+      sum += potentialMultiplier;
+    }
+  }
+
   cout << "Sum: " << sum << endl;
   return 0;
+}
+
+string findSymbol(const vector<vector<char>>& grid, int i, int j) {
+  int rows[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+  int cols[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+  string symbol = "";
+  
+  for (int k = 0; k < 8; k++) {
+    int newRow = i + rows[k];
+    int newCol = j + cols[k];
+    if (isSafeToCheckNeighbor(newRow, newCol, grid) && isSymbol(grid[newRow][newCol])) {
+      cout << "Symbol found at " << newRow << "," << newCol << endl;
+      symbol = to_string(newRow) + "," + to_string(newCol);
+    }
+  }
+  return symbol;
 }
 
 bool neighborContainsSymbol(const vector<vector<char>>& grid, int i, int j) {
@@ -64,7 +103,7 @@ bool isSafeToCheckNeighbor(int i, int j, const vector<vector<char>>& grid) {
 }
 
 bool isSymbol(char c) {
-  if (!isdigit(c) && c != '.') {
+  if (c == '*') {
     return true;
   } else {
     //cout << c << " is not a symbol!!" << endl;
